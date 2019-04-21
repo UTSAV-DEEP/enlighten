db946fe2c12e2c59b200496dd3f58c92218db90f
Utsav Deep <utsav@circles.asia>
2019-04-21 23:34:11 +0800

    initial commit

---
+++ src/main/java/com/utsav/enlighten/EnlightenApplication.java
@@ -0,0 +1,13 @@
+package com.utsav.enlighten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnlightenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnlightenApplication.class, args);
+	}
+
+}
